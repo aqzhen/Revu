52c57d56ef161ccdeb0c776478f10e8c4f183354
Andrew Zhen <43920774+aqzhen@users.noreply.github.com>
2024-03-06 15:39:17 -0500

    set up support for adding reviews to prisma database

--- app/globals.d.ts
+++ app/globals.d.ts
@@ -1,1 +1,13 @@
 declare module "*.css";
+
+export type Review = {
+  reviewerName: string;
+  reviewerExternalId: number;
+  createdAt: string;
+  updatedAt: string;
+  verified: string;
+  reviewId: number;
+  rating: number;
+  title: string;
+  body: string;
+};
--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
 import {
   useActionData,
   useNavigation,
@@ -18,111 +17,98 @@ import {
   Spinner,
 } from "@shopify/polaris";
 import { authenticate } from "../shopify.server";
+import { parseReviewData } from "./metafield_parsers/judge";
+import { getProducts, fetchJudgeReviews } from "./backend/api_calls";
+import { addReviewsToDatabase } from "./backend/prisma/helpers";
+import { Review } from "../globals";
+import { log } from "console";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const { admin } = await authenticate.admin(request);
 
   console.log("Loading products");
-
-  const response = await admin.graphql(
-    `#graphql
-    query {
-      products(first: 20) {
-        edges {
-          node {
-            id
-            title
-            images (first: 1) {
-              edges {
-                node {
-                  url
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    `,
-  );
-  const responseJson = await response.json();
-
-  return json({
-    products: responseJson.data?.products?.edges,
-  });
+  return getProducts(admin);
 };
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const { admin } = await authenticate.admin(request);
-
   const formData = await request.formData();
+  var apiQuery = formData.get("apiQuery") as string;
   var productId = formData.get("productId") as string;
+  var reviews = formData.get("reviews") as string;
+
   console.log(productId);
 
-  const response = await admin.graphql(
-    `#graphql
-    query($productId: ID!) {
-      product(id: $productId) {
-        id
-        metafield(namespace: "judgeme", key: "widget") {
-          value
-        }
-      }
-    }
-    `,
-    {
-      variables: {
-        productId: "gid://shopify/Product/" + productId,
-      },
-    },
-  );
-  const responseJson = await response.json();
-  console.log(responseJson);
-
-  return json({
-    metafield: responseJson.data?.product?.metafield?.value,
-  });
+  if (apiQuery === "fetchJudgeReviews") {
+    return fetchJudgeReviews(productId);
+  } else if (apiQuery === "addReviewsToDatabase") {
+    addReviewsToDatabase(Number(productId), JSON.parse(reviews || "[]"));
+    return null;
+  }
 };
 
 export default function Index() {
   const [products, setProducts] = useState<any[]>([]);
-  var [selectedProduct, setSelectedProduct] = useState(null);
+  var [selectedProduct, setSelectedProduct] = useState<number>();
+  var [reviewDetails, setReviewDetails] = useState<Review[]>([]);
 
   // get metafield data
   const nav = useNavigation();
-  const metafieldData = useActionData<typeof action>();
+  const reviewData = useActionData<typeof action>();
   const submit = useSubmit();
   const isLoading =
     ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
 
   // parse the metafield data
-
-  // get products data on load
-  const productsData = useLoaderData<typeof loader>();
-
   useEffect(() => {
-    setProducts(
-      productsData.products.map((edge: any) => ({
-        id: edge?.node?.id,
-        title: edge?.node?.title,
-        imageUrl: edge?.node?.images?.edges?.[0]?.node?.url,
-      })),
-    );
-  }, []);
+    if (reviewData && reviewData?.reviews) {
+      var parsedData = parseReviewData(reviewData?.reviews);
+      setReviewDetails(parsedData);
+    }
+  }, [reviewData]);
 
-  // trigger action to get metafield
-  const getMetafield = async (selectedProductId: any) => {
+  // trigger action to get reviews
+  const getReviews = async (selectedProductId: Number) => {
     await submit(
-      { productId: Number(selectedProductId) },
+      { apiQuery: "fetchJudgeReviews", productId: Number(selectedProductId) },
       { replace: true, method: "POST" },
     );
   };
 
+  const pushReviewsToDatabase = async (
+    productId: number,
+    reviews: Review[],
+  ) => {
+    await submit(
+      {
+        apiQuery: "addReviewsToDatabase",
+        productId: productId,
+        reviews: JSON.stringify(reviews),
+      },
+      { replace: true, method: "POST" },
+    );
+  };
+
+  // PRODUCTS
+  // get products data on load
+  const productsData = useLoaderData<typeof loader>();
+
+  useEffect(() => {
+    if (productsData?.products) {
+      setProducts(
+        productsData.products.map((edge: any) => ({
+          id: edge?.node?.id,
+          title: edge?.node?.title,
+          imageUrl: edge?.node?.images?.edges?.[0]?.node?.url,
+        })),
+      );
+    }
+  }, []);
+
   // set selected product
-  const handleProductSelection = async (productId: any) => {
+  const handleProductSelection = async (productId: string) => {
     var trimmed_id = productId.replace("gid://shopify/Product/", "");
-    setSelectedProduct(trimmed_id);
-    getMetafield(trimmed_id);
+    setSelectedProduct(Number(trimmed_id));
+    getReviews(Number(trimmed_id));
   };
 
   return (
@@ -147,6 +133,17 @@ export default function Index() {
 
       <Card>
         <p>Selected Product ID: {selectedProduct}</p>
+        {
+          <Button
+            onClick={() =>
+              selectedProduct &&
+              reviewDetails.length != 0 &&
+              pushReviewsToDatabase(selectedProduct, reviewDetails)
+            }
+          >
+            Add Reviews to Database
+          </Button>
+        }
       </Card>
 
       {isLoading ? (
@@ -154,9 +151,22 @@ export default function Index() {
           <Spinner size="small" />
           <p>Loading...</p>
         </Card>
-      ) : metafieldData?.metafield ? (
+      ) : reviewData?.reviews ? (
         <Card>
-          <p>{metafieldData.metafield}</p>
+          <p>Reviews:</p>
+          {reviewDetails.map((review, index) => (
+            <Card key={index}>
+              <p>Reviewer Name: {review.reviewerName}</p>
+              <p>Reviewer External ID: {review.reviewerExternalId}</p>
+              <p>Created At: {review.createdAt}</p>
+              <p>Updated At: {review.updatedAt}</p>
+              <p>Verified: {review.verified}</p>
+              <p>Review ID: {review.reviewId}</p>
+              <p>Rating: {review.rating}</p>
+              <p>Review Title: {review.title}</p>
+              <p>Review Body: {review.body}</p>
+            </Card>
+          ))}
         </Card>
       ) : null}
     </Page>
