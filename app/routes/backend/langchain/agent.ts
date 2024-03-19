3edf22c59a02078874cc091db05b1fa00eeb1558
Andrew Zhen <43920774+aqzhen@users.noreply.github.com>
2024-03-19 13:19:22 -0400

    hooked up LLM to end of agent to interpret returned reviewIds

--- app/routes/backend/langchain/agent.ts
+++ app/routes/backend/langchain/agent.ts
@@ -4,16 +4,24 @@ import {
   MessagesPlaceholder,
 } from "@langchain/core/prompts";
 import { ChatOpenAI } from "@langchain/openai";
-import { json } from "@remix-run/node";
 import fs from "fs";
 import { AgentExecutor, createOpenAIToolsAgent } from "langchain/agents";
 import { SqlToolkit } from "langchain/agents/toolkits/sql";
 import { AIMessage } from "langchain/schema";
 import { SqlDatabase } from "langchain/sql_db";
+import { BufferMemory } from "langchain/memory";
 import { DataSource } from "typeorm";
 import { addQueryToSingleStore } from "../vectordb/helpers";
+import { log } from "console";
+import { createLogger } from "vite";
+import { json } from "@remix-run/node";
 
 let executor: AgentExecutor;
+const llm = new ChatOpenAI({
+  modelName: "gpt-3.5-turbo",
+});
+let db: SqlDatabase;
+
 export async function initialize_agent() {
   try {
     const dataSource = new DataSource({
@@ -27,61 +35,14 @@ export async function initialize_agent() {
         ca: fs.readFileSync("./singlestore_bundle.pem"),
       },
     });
-
-    const db = await SqlDatabase.fromDataSourceParams({
+    db = await SqlDatabase.fromDataSourceParams({
       appDataSource: dataSource,
     });
     const toolkit = new SqlToolkit(db);
     const tools = toolkit.getTools();
-    const llm = new ChatOpenAI({
-      modelName: "gpt-3.5-turbo",
-    });
-
-    // \n Example 1:
-    // Q: QueryId: 1234. What is the number of reviews that describe being beginners at snowboarding?
-    // A:
-
-    // \nThought: I should use the DOT_PRODUCT function to calculate the similarity between the embedding of the query and the bodyEmbedding of the review. Then I can count the number of reviews that have a similarity greater than 0.8 to the query.
-    // \nAction: query-sql
-    // \nAction Input:
-    //     SELECT COUNT(*)
-    //     FROM review
-    //     WHERE DOT_PRODUCT(bodyEmbedding, (SELECT embedding FROM queries WHERE queryId = 1234)) > 0.8;
-    // \nObservation: 5
-    // \nThought: I now know the final answer
-    // \nFinal Answer: 5
-
-    // const prefix = `
-    // You are an agent designed to interact with a SQL database called SingleStore. This sometimes has Shard and Sort keys in the table schemas, which you can ignore.
-    // \nGiven an input question, create a syntactically correct MySQL query to run, then look at the results of the query and return the answer.
-    // \n If you are asked about similarity questions, you should use the DOT_PRODUCT function. The table has one column with vector data called bodyEmbedding.
-    // The DOT_PRODUCT function takes two vectors and returns the dot product of the two vectors. The result is a scalar value.
-    // The DOT_PRODUCT function is used to calculate the similarity between two vectors. The result is a scalar value. The higher the value, the more similar the two vectors are. The lower the value, the less similar the two vectors are.
-
-    // The first piece of information you will be given will be the queryID. This is a unique number identifier for the query. You should read the queryID and use it to query the queries table. When deciding if a query is similar to the body of a review, you should use the DOT_PRODUCT function to calculate the similarity between the embedding of the query and the bodyEmbedding of the review.
-
-    // If the query is asking you to return information about reviews that are similar to the query, you should use the DOT_PRODUCT function to calculate the similarity between the embedding of the query and the bodyEmbedding of the review. You should then return the reviews that have the highest similarity to the query. You should return the reviews in descending order of similarity. You should return at most {top_k} reviews. If there are no reviews that are similar to the query, you should return an empty list.
-
-    // Anytime you are enticed to use the WHERE function, you should use the DOT_PRODUCT function instead of LIKE to compute the similarity between the
-    // embedding of the query and the bodyEmbedding of the review.
-
-    // \nIf you are asked to describe the database, you should run the query SHOW TABLES
-    // \nUnless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results.
-    // \nYou can order the results by a relevant column to return the most interesting examples in the database.
-    // \nNever query for all the columns from a specific table, only ask for the relevant columns given the question.
-    // \n You have access to tools for interacting with the database. Only use the below tools.Only use the information returned by the below tools to construct your final answer. These are the tools: [query-sql, list-tables-sql]
-    // \nYou MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again up to 3 times.
-    // \n\nDO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
-    // \n\nIf the question does not seem related to the database, just return "I don\'t know" as the answer.\n,
-    // `;
-    // const suffix = `
-    // Use the following format:\n
-    // \nQuestion: the input question you must answer
-    // \nFinal Answer: the final answer to the original input question
-    // \nSQL Query used to get the Answer: the final sql query used for the final answer
-    // `;
 
     const prefix = `
+
     You are an agent designed to interact with a SQL database.
     Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
     If you are asked to describe the database, you should run the query SHOW TABLES
@@ -102,7 +63,7 @@ export async function initialize_agent() {
     If you are asked anything about the content of the review, you should use the DOT_PRODUCT function to calculate the similarity between the semanticEmbedding of the query, which is found in the Queries table, and the bodyEmbedding of the review.
     Use the DOT_PRODUCT function on the body column as you normally would when using the WHERE...LIKE functionality in SQL. You should NEVER return the review body in the final answer.
 
-    When unsure about what to return, return the reviewID and the similarity score in the final answer by default.
+    Always eturn the reviewID and the similarity score in the final answer by default.
 
     \n Example 1:
     Q: QueryId: 1234. What is the number of reviews that describe being beginners at snowboarding?
@@ -114,20 +75,31 @@ export async function initialize_agent() {
     \nAction: query-sql
     \nAction Input:
     SELECT COUNT(*) AS num_rows,
-        DOT_PRODUCT(review.bodyEmbedding, query.semanticEmbedding) AS similarity_score
+        DOT_PRODUCT(Review.bodyEmbedding, Query.semanticEmbedding) AS similarity_score
     FROM Review
-    CROSS JOIN (SELECT semanticEmbedding FROM Queries WHERE queryId = 1234) AS query
+    CROSS JOIN (SELECT semanticEmbedding FROM Queries WHERE queryId = 1234) AS Query
     ORDER BY similarity_score DESC; 
     LIMIT 5;
 
-    \nObservation: 5
-    \nThought: I now know the final answer
-    \nFinal Answer: 5
-    `;
 
+    \n Example 2:
+    Q: QueryId: 1234. Is this board good for beginners?
+
+    \nThought: I should use the DOT_PRODUCT function to calculate the similarity between the embedding of the query and the bodyEmbedding of the review. 
+    Then I can return the top most similar reviews in order of their similarity rank. Then, I can return the reviewIDs of the reviews that are most similar
+    to the query. 
+
+    \nAction: query-sql
+    \nAction Input:
+    SELECT reviewId,
+        DOT_PRODUCT(Review.bodyEmbedding, Query.semanticEmbedding) AS similarity_score
+    FROM Review
+    CROSS JOIN (SELECT semanticEmbedding FROM Queries WHERE queryId = 1234) AS Query
+    ORDER BY similarity_score DESC; 
+    LIMIT 5;
+    `;
     const suffix = `
     Begin!
-
     Question: {input}
     Thought: I should look at the tables in the database to see what I can query.
     {agent_scratchpad}
@@ -135,10 +107,16 @@ export async function initialize_agent() {
 
     const prompt = ChatPromptTemplate.fromMessages([
       ["system", prefix],
+      // new MessagesPlaceholder("chat_history"),
       HumanMessagePromptTemplate.fromTemplate("{input}"),
       new AIMessage(suffix.replace("{agent_scratchpad}", "")),
       new MessagesPlaceholder("agent_scratchpad"),
     ]);
+    // const memory = new BufferMemory({
+    //   memoryKey: "chat_history",
+    //   returnMessages: true,
+    // }); // adding memory here
+
     const newPrompt = await prompt.partial({
       dialect: toolkit.dialect,
       top_k: "10",
@@ -153,6 +131,7 @@ export async function initialize_agent() {
       tools,
       returnIntermediateSteps: true,
       verbose: false,
+      // memory: memory,
     });
   } catch (err) {
     console.error("ERROR", err);
@@ -184,14 +163,36 @@ export async function call_agent(query: string) {
         response.sqlQuery = step.action.toolInput.input;
         response.result = step.observation;
       }
-
-      console.log("HIII");
-      console.log(
-        `Intermediate steps ${JSON.stringify(result.intermediateSteps, null, 2)}`,
-      );
     });
-    response.output = result.output;
-    console.log(response);
+    console.log(
+      `Intermediate steps ${JSON.stringify(result.intermediateSteps, null, 2)}`,
+    );
+    // parse result to perform additional queries and LLM calls
+    // if results has reviewIds and similarity_score, then we perform query to grab bodies and feed into LLM
+    let llmOutput;
+    const resultObject = JSON.parse(response.result as unknown as string);
+    if (resultObject.length > 0 && (resultObject[0] as any).reviewId) {
+      // get reviewIds
+      const reviewIds = resultObject.map((r: any) => r.reviewId);
+      // get review bodies
+      const reviewBodies = await db.run(
+        `SELECT reviewId, body FROM Review WHERE reviewId IN (${reviewIds.join(
+          ",",
+        )})`,
+      );
+
+      llmOutput = (
+        await llm.invoke(
+          "Using the following reviews, answer this query: " +
+            query +
+            "\n" +
+            reviewBodies,
+        )
+      ).content;
+    }
+
+    response.output = llmOutput + "\n" + result.output;
+    console.log(response.output);
     return json(response);
   } catch (err) {
     console.error("ERROR", err);
